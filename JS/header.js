276131b38d59999f329178264ba32d59bab0d871
malahim-mar <armalahim01@gmail.com>
2025-08-15 22:13:00 +0500

    Header Dynamic Done

--- JS/header.js
+++ JS/header.js
@@ -1,2 +1,23 @@
 const header = document.getElementById("header");
-header.innerHTML = ` `;
+header.innerHTML = `     <header class="header">
+      <div>
+        <nav class="menu"> 
+          <ul class="menu-list">
+            <li class="menu-list-item"><a href="#">About</a></li>
+            <li class="menu-list-item"><a href="#">Services</a></li>
+            <li class="menu-list-item"><a href="#">Portfolio</a></li>
+            <li class="menu-list-item"><a href="#">Hire Me</a></li>
+            <li class="menu-list-item"><a href="#">Blogs</a></li>
+          </ul>
+        </nav>
+      </div>
+      <div>
+        <h3 class="text-logo">Malahim's Development</h1>
+      </div>
+      <div>
+        <button class="global-btn">
+          Connect With US
+          <i class='fas fa-arrow-alt-circle-right' style='font-size:24px; color: white;'></i>
+        </button>
+      </div>
+    </header>`;
